c7cda9f99cc910f2716997a4f748c92f393b45f8
unknown <satyam0507@gmail.com>
2018-04-04 13:28:29 +0530

    corrected service worker route in index.js

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 4444));
+app.get('/service-worker.js', function(req, res) {
+    var options = {
+        root: __dirname + '/',
+        headers: {
+            'x-timestamp': Date.now(),
+            'x-sent': true
+        }
+    };
 
-app.use(express.static('/'))
+
+    res.sendFile('service-worker.js', options, function(err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+});
+
 app.get('/:sub/:name', function(req, res) {
     var fileName = req.params.name;
     var sub = req.params.sub||'Home';
